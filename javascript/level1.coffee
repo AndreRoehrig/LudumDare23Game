857fa1dbaadba091425f4e1eedefdf1fa8651f94
André Röhrig <andre.roehrig@gmail.com>
2012-04-21 12:51:40 +0200

    image

--- javascript/level1.coffee
+++ javascript/level1.coffee
@@ -1,19 +1,36 @@
 gamejs = require("gamejs")
-gamejs.preload(["images/char.png","images/runner.png"])
+gamejs.preload(["images/char.png","images/wall.png"])
+
+#globals…
+
+char_x = 0
+char_y = 0
+char_ver_acc = 0
+char_ver_speed = 0
+char_hor_acc = 0
+char_hor_speed = 0
+
+
+charSprite = new gamejs.sprite.Sprite()
+charSprite.rect = new gamejs.Rect([250,500])
 
 display = gamejs.display.setMode([800,600])
 
 draw = ->
     display.clear()
-    runner = gamejs.image.load("images/char.png")
     charSprite.draw(display)
 
 charSprite = new gamejs.sprite.Sprite()
 charSprite.rect = new gamejs.Rect([250,500])
 
+handleEvent = (event) ->
+    if event.key == gamejs.event.K_UP    and event.type == gamejs.event.KEY_DOWN then char_ver_acc += 1
+    if event.key == gamejs.event.K_DOWN  and event.type == gamejs.event.KEY_DOWN then char_ver_acc -= 1
+    if event.key == gamejs.event.K_RIGHT and event.type == gamejs.event.KEY_DOWN then char_hor_acc += 1
+    if event.key == gamejs.event.K_LEFT  and event.type == gamejs.event.KEY_DOWN then char_hor_acc -= 1
 main = ->
     
-    charSprite.image = gamejs.image.load("images/runner.png")
+    charSprite.image = gamejs.image.load("images/char.png")
     gamejs.time.fpsCallback(draw, this, 30)
 
 
--- javascript/level1.js
+++ javascript/level1.js
@@ -1,16 +1,30 @@
 (function() {
-  var charSprite, display, draw, gamejs, main;
+  var charSprite, char_hor_acc, char_hor_speed, char_ver_acc, char_ver_speed, char_x, char_y, display, draw, gamejs, handleEvent, main;
 
   gamejs = require("gamejs");
 
-  gamejs.preload(["images/char.png", "images/runner.png"]);
+  gamejs.preload(["images/char.png", "images/wall.png"]);
+
+  char_x = 0;
+
+  char_y = 0;
+
+  char_ver_acc = 0;
+
+  char_ver_speed = 0;
+
+  char_hor_acc = 0;
+
+  char_hor_speed = 0;
+
+  charSprite = new gamejs.sprite.Sprite();
+
+  charSprite.rect = new gamejs.Rect([250, 500]);
 
   display = gamejs.display.setMode([800, 600]);
 
   draw = function() {
-    var runner;
     display.clear();
-    runner = gamejs.image.load("images/char.png");
     return charSprite.draw(display);
   };
 
@@ -18,8 +32,23 @@
 
   charSprite.rect = new gamejs.Rect([250, 500]);
 
+  handleEvent = function(event) {
+    if (event.key === gamejs.event.K_UP && event.type === gamejs.event.KEY_DOWN) {
+      char_ver_acc += 1;
+    }
+    if (event.key === gamejs.event.K_DOWN && event.type === gamejs.event.KEY_DOWN) {
+      char_ver_acc -= 1;
+    }
+    if (event.key === gamejs.event.K_RIGHT && event.type === gamejs.event.KEY_DOWN) {
+      char_hor_acc += 1;
+    }
+    if (event.key === gamejs.event.K_LEFT && event.type === gamejs.event.KEY_DOWN) {
+      return char_hor_acc -= 1;
+    }
+  };
+
   main = function() {
-    charSprite.image = gamejs.image.load("images/runner.png");
+    charSprite.image = gamejs.image.load("images/char.png");
     return gamejs.time.fpsCallback(draw, this, 30);
   };
 

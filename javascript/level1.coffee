5e3d9ba5851f4cc6e500590d4e5513c1b1c70ca2
André Röhrig <andre.roehrig@gmail.com>
2012-04-21 14:17:04 +0200

    control added

--- javascript/level1.coffee
+++ javascript/level1.coffee
@@ -2,6 +2,11 @@ gamejs = require("gamejs")
 gamejs.preload(["images/char.png","images/wall.png"])
 
 #globals…
+key_down = 0
+key_up = 0
+key_left = 0
+key_right = 0
+
 
 char_x = 0
 char_y = 0
@@ -16,22 +21,45 @@ charSprite.rect = new gamejs.Rect([250,500])
 
 display = gamejs.display.setMode([800,600])
 
+
+ 
 draw = ->
+    if key_up == 1 then char_ver_acc -= 0.3
+    if key_down == 1 then char_ver_acc += 0.3
+    if key_right == 1 then char_hor_acc += 0.3
+    if key_left == 1 then char_hor_acc -= 0.3
+    handleEvent(event) for event in gamejs.event.get()
+    char_ver_acc = char_ver_acc / 1.3
+    char_hor_acc = char_hor_acc / 1.3
     display.clear()
+    char_ver_speed += char_ver_acc
+    char_ver_speed = char_ver_speed / 1.3
+    char_x += char_hor_speed
+    char_hor_speed += char_hor_acc
+    char_hor_speed = char_hor_speed / 1.3
+    char_y += char_ver_speed
+    charSprite.rect = new gamejs.Rect([char_x,char_y])
     charSprite.draw(display)
 
 charSprite = new gamejs.sprite.Sprite()
-charSprite.rect = new gamejs.Rect([250,500])
+charSprite.rect = new gamejs.Rect([char_ver_acc,500])
 
 handleEvent = (event) ->
-    if event.key == gamejs.event.K_UP    and event.type == gamejs.event.KEY_DOWN then char_ver_acc += 1
-    if event.key == gamejs.event.K_DOWN  and event.type == gamejs.event.KEY_DOWN then char_ver_acc -= 1
-    if event.key == gamejs.event.K_RIGHT and event.type == gamejs.event.KEY_DOWN then char_hor_acc += 1
-    if event.key == gamejs.event.K_LEFT  and event.type == gamejs.event.KEY_DOWN then char_hor_acc -= 1
+    
+    if event.key == gamejs.event.K_UP    and event.type == gamejs.event.KEY_DOWN then key_up = 1
+    if event.key == gamejs.event.K_DOWN  and event.type == gamejs.event.KEY_DOWN then key_down = 1
+    if event.key == gamejs.event.K_RIGHT and event.type == gamejs.event.KEY_DOWN then key_right = 1
+    if event.key == gamejs.event.K_LEFT  and event.type == gamejs.event.KEY_DOWN then key_left = 1
+    if event.key == gamejs.event.K_UP    and event.type == gamejs.event.KEY_UP then key_up = 0
+    if event.key == gamejs.event.K_DOWN  and event.type == gamejs.event.KEY_UP then key_down = 0
+    if event.key == gamejs.event.K_RIGHT and event.type == gamejs.event.KEY_UP then key_right = 0
+    if event.key == gamejs.event.K_LEFT  and event.type == gamejs.event.KEY_UP then key_left = 0
+
 main = ->
     
     charSprite.image = gamejs.image.load("images/char.png")
-    gamejs.time.fpsCallback(draw, this, 30)
+    gamejs.time.fpsCallback(draw, this, 60)
+   
 
 
 gamejs.ready(main)
--- javascript/level1.js
+++ javascript/level1.js
@@ -1,9 +1,17 @@
 (function() {
-  var charSprite, char_hor_acc, char_hor_speed, char_ver_acc, char_ver_speed, char_x, char_y, display, draw, gamejs, handleEvent, main;
+  var charSprite, char_hor_acc, char_hor_speed, char_ver_acc, char_ver_speed, char_x, char_y, display, draw, gamejs, handleEvent, key_down, key_left, key_right, key_up, main;
 
   gamejs = require("gamejs");
 
   gamejs.preload(["images/char.png", "images/wall.png"]);
+
+  key_down = 0;
+
+  key_up = 0;
+
+  key_left = 0;
+
+  key_right = 0;
 
   char_x = 0;
 
@@ -24,32 +32,63 @@
   display = gamejs.display.setMode([800, 600]);
 
   draw = function() {
+    var event, _i, _len, _ref;
+    if (key_up === 1) char_ver_acc -= 0.3;
+    if (key_down === 1) char_ver_acc += 0.3;
+    if (key_right === 1) char_hor_acc += 0.3;
+    if (key_left === 1) char_hor_acc -= 0.3;
+    _ref = gamejs.event.get();
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      event = _ref[_i];
+      handleEvent(event);
+    }
+    char_ver_acc = char_ver_acc / 1.3;
+    char_hor_acc = char_hor_acc / 1.3;
     display.clear();
+    char_ver_speed += char_ver_acc;
+    char_ver_speed = char_ver_speed / 1.3;
+    char_x += char_hor_speed;
+    char_hor_speed += char_hor_acc;
+    char_hor_speed = char_hor_speed / 1.3;
+    char_y += char_ver_speed;
+    charSprite.rect = new gamejs.Rect([char_x, char_y]);
     return charSprite.draw(display);
   };
 
   charSprite = new gamejs.sprite.Sprite();
 
-  charSprite.rect = new gamejs.Rect([250, 500]);
+  charSprite.rect = new gamejs.Rect([char_ver_acc, 500]);
 
   handleEvent = function(event) {
     if (event.key === gamejs.event.K_UP && event.type === gamejs.event.KEY_DOWN) {
-      char_ver_acc += 1;
+      key_up = 1;
     }
     if (event.key === gamejs.event.K_DOWN && event.type === gamejs.event.KEY_DOWN) {
-      char_ver_acc -= 1;
+      key_down = 1;
     }
     if (event.key === gamejs.event.K_RIGHT && event.type === gamejs.event.KEY_DOWN) {
-      char_hor_acc += 1;
+      key_right = 1;
     }
     if (event.key === gamejs.event.K_LEFT && event.type === gamejs.event.KEY_DOWN) {
-      return char_hor_acc -= 1;
+      key_left = 1;
+    }
+    if (event.key === gamejs.event.K_UP && event.type === gamejs.event.KEY_UP) {
+      key_up = 0;
+    }
+    if (event.key === gamejs.event.K_DOWN && event.type === gamejs.event.KEY_UP) {
+      key_down = 0;
+    }
+    if (event.key === gamejs.event.K_RIGHT && event.type === gamejs.event.KEY_UP) {
+      key_right = 0;
+    }
+    if (event.key === gamejs.event.K_LEFT && event.type === gamejs.event.KEY_UP) {
+      return key_left = 0;
     }
   };
 
   main = function() {
     charSprite.image = gamejs.image.load("images/char.png");
-    return gamejs.time.fpsCallback(draw, this, 30);
+    return gamejs.time.fpsCallback(draw, this, 60);
   };
 
   gamejs.ready(main);
